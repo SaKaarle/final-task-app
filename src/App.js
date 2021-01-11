9eac74c102700ea51924a9348fc4e13dee599126
SaKaarle <saku.kaarlejarvi@gmail.com>
2021-01-11 08:49:45 +0200

    Tehtävä 1 suoritettu

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+//import { Toolbar, Typography } from "@material-ui/core";
+//import AppBar from '@material-ui/core/AppBar';
 import './App.css';
+import CustomerList from './components/CustomerList';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>test</h1>
+      <CustomerList />
     </div>
   );
 }
---
+++ src/components/CustomerList.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+import { AgGridReact } from "ag-grid-react";
+
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-material.css";
+//ag-theme material on tärkeä, enemmän asetuksia
+
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from "@material-ui/icons/Delete";
+import Snackbar from "@material-ui/core/Snackbar";
+
+function CustomerList() {
+    const[henk, setHenk] = useState([]);
+    const[open, setOpen] = React.useState(false);
+
+    useEffect(()=>{
+        getHenkilot();
+        //console.log(henk)
+    },[]);
+
+    const handleOpen=()=>{
+        setOpen(true);
+    };
+    const handleClose=()=>{
+        setOpen(false);
+    };
+    
+    const getHenkilot = () => {
+        fetch("https://customerrest.herokuapp.com/api/customers")
+        .then((response) => response.json())
+        .then((data) => setHenk(data.content))
+        .catch((err) => console.error(err));
+        console.log(henk)
+    };
+
+const columns = [
+
+    {field: "firstname",sortable:true,filter:true},
+    {field: "lastname",sortable:true,filter:true},
+    {field: "streetaddress",sortable:true,filter:true},
+    {field: "postcode",sortable:true,filter:true},
+    {field: "city",sortable:true,filter:true},
+    {field: "email",sortable:true,filter:true},
+    {field: "phone",sortable:true,filter:true},
+
+
+]
+
+return(
+    <div> 
+    <div
+      className="ag-theme-material"
+      style={{ height: 600, width: "90%", margin: "auto" }}
+    >
+    <AgGridReact
+    rowData={henk}
+    columnDefs={columns}
+    pagination="true"
+    paginationPageSize="10"
+    ></AgGridReact>
+
+    </div>
+    <Snackbar open={open}
+    onClose={handleClose}
+    autoHideDuration={2500}
+    message="Test"/>
+
+    </div>
+)
+    
+}
+export default CustomerList;
